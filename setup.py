c61ba3187cef636bdd98258171b358cd16fc8c8a
BabylooPro <babyloopro@gmail.com>
2025-02-20 03:44:47 +0100

    added(workflow): 3.10 python version to test job metrix

--- setup.py
+++ setup.py
@@ -68,12 +68,15 @@ setup(
         "Bug Tracker": "https://github.com/BabylooPro/Open-AutoTools/issues",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
-    
+    python_requires=">=3.10",
+
+    # ENTRY POINTS FOR CLI COMMANDS
     entry_points='''
         [console_scripts]
         autotools=autotools.cli:cli
