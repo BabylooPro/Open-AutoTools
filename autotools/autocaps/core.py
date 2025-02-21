d8dbd5ad5de1d75d9c9a263cde848159f9529c1d
BabylooPro <babyloopro@gmail.com>
2025-02-21 03:25:09 +0100

    fixed: handle pyperclip clipboard errors in headless environments

--- autotools/autocaps/core.py
+++ autotools/autocaps/core.py
@@ -3,5 +3,8 @@ import pyperclip
 # AUTOCAPS CORE FUNCTION DEFINITION
 def autocaps_transform(text):
     transformed_text = text.upper()  # TRANSFORM TEXT TO UPPERCASE
-    pyperclip.copy(transformed_text)  # COPY TRANSFORMED TEXT TO CLIPBOARD
+    try:
+        pyperclip.copy(transformed_text)  # COPY TRANSFORMED TEXT TO CLIPBOARD
+    except pyperclip.PyperclipException:
+        pass  # IGNORE CLIPBOARD ERRORS IN HEADLESS ENVIRONMENT
     return transformed_text
--- autotools/autolower/core.py
+++ autotools/autolower/core.py
@@ -3,5 +3,8 @@ import pyperclip
 # AUTOLOWERCASE CORE FUNCTION DEFINITION
 def autolower_transform(text):
     transformed_text = text.lower()  # TRANSFORM TEXT TO LOWERCASE
-    pyperclip.copy(transformed_text)  # COPY TRANSFORMED TEXT TO CLIPBOARD
+    try:
+        pyperclip.copy(transformed_text)  # COPY TRANSFORMED TEXT TO CLIPBOARD
+    except pyperclip.PyperclipException:
+        pass  # IGNORE CLIPBOARD ERRORS IN HEADLESS ENVIRONMENT
     return transformed_text
